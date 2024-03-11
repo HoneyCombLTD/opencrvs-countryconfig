ac4c7b6c665931ee916a00db15ef826369b7cf4b
anyric <anyamaronyango@gmail.com>
2024-03-11 14:43:40 +0300

    update Docker file

--- start-prod.sh
+++ start-prod.sh
@@ -15,5 +15,4 @@ sed -i "s/{{hostname}}/$DOMAIN/g" src/login-config.prod.js
 sed -i "s={{sentry}}=$SENTRY_DSN=g" src/client-config.prod.js
 sed -i "s={{sentry}}=$SENTRY_DSN=g" src/login-config.prod.js
 
-# yarn start:prod
-yarn start
+yarn start:prod
